061a897f149ad0428d0db79eaa4f2b3555bca423
Óscar Miranda <oscarmiranda3615@gmail.com>
2022-09-01 17:32:20 -0600

    add base layout

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,19 @@
 import { Controller, Get, Render } from '@nestjs/common';
+import { ProductsService } from './products/products.service';
 
 @Controller()
 export class AppController {
+	constructor(private products: ProductsService) {}
+
 	@Get()
 	@Render('index')
-	index() {
-		return { message: 'Hello, world!' };
+	async index() {
+		return {
+			products: await this.products.findAll()
+		};
 	}
+
+	@Get('/new')
+	@Render('new-product')
+	async newProduct() {}
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './product.entity';
 
+const products: Product[] = [
+	{ id: 1, nombre: 'Producto 1', precio: 20 },
+	{ id: 2, nombre: 'Producto 2', precio: 20 },
+	{ id: 3, nombre: 'Producto 3', precio: 20 },
+	{ id: 4, nombre: 'Producto 4', precio: 20 },
+	{ id: 5, nombre: 'Producto 5', precio: 20 }
+];
+
 @Injectable()
 export class ProductsService {
 	constructor(
@@ -11,7 +19,8 @@ export class ProductsService {
 	) {}
 
 	findAll(): Promise<Product[]> {
-		return this.repo.find();
+		return Promise.resolve(products);
+		// return this.repo.find();
 	}
 
 	findById(id: number): Promise<Product> {
